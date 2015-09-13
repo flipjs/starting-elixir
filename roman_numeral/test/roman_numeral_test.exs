5f2e61c01c606a9a30e2aefe71392fae82626212
Felipe Apostol <flipjs.io@gmail.com>
2015-09-13 09:14:40 +0100

    initial commit

---
+++ roman_numeral/test/roman_numeral_test.exs
@@ -0,0 +1,23 @@
+defmodule RomanNumeralTest do
+  use ExUnit.Case
+
+  test "converts arabic to roman numerals" do
+    test_values = [
+      {0, ""},
+      {1, "I"},
+      {2, "II"},
+      {3, "III"},
+      {4, "IV"},
+      {5, "V"},
+      {6, "VI"},
+      {7, "VII"},
+      {8, "VIII"},
+      {9, "IX"},
+      {10, "X"},
+      {28, "XXVIII"}
+    ]
+
+    Enum.each test_values, fn({arabic, roman}) -> assert RomanNumeral.converts(arabic) == roman end
+  end
+
+end
